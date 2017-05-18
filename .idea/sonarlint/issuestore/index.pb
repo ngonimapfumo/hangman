0f8d3e45fba82d3d60996de6bbfd0d6ab08dd968
ngonimapfumo <ngonimapfumo@hotmail.com>
2017-05-18 08:38:56 +0000

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+;
+src/hang.py,c/5/c502eba12f833368c6bd424f1cbef5e3ff21ba25
---
+++ src/hang.py
@@ -0,0 +1,46 @@
+import random
+
+
+def hangman(word):
+    wrong_guesses = 0
+    stages = ["", "________      ", "|      |      ", "|      0      ", "|     /|\     ", "|     / \     ", "|"]
+    remaining_letters = list(word)
+    letter_board = ["__"] * len(word)
+    win = False
+    print('Welcome')
+    while wrong_guesses < len(stages) - 1:
+        print('\n')
+        guess = input("Guess a letter")
+        if guess in remaining_letters:
+            character_index = remaining_letters.index(guess)
+            letter_board[character_index] = guess
+            remaining_letters[character_index] = '$'
+        else:
+            wrong_guesses += 1
+        print((' '.join(letter_board)))
+        print('\n'.join(stages[0: wrong_guesses + 1]))
+        if '__' not in letter_board:
+            print('You win! The word was:')
+            print(' '.join(letter_board))
+            win = True
+            break
+    if not win:
+        print('\n'.join(stages[0: wrong_guesses]))
+        print('You lose! The words was {}'.format(word))
+
+
+def random_words():
+    word = {""}
+    random_word = random.randint(0,2)
+    if random_word == 0:
+        word = "cat"
+
+    elif random_word == 1:
+        word = "shoe"
+
+    elif random_word == 2:
+        word = "body"
+
+    return word
+
+hangman(random_words())
